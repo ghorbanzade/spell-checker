49ce1ea8d4b91d5fc95c098c96bd4d9b350242c7
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-16 19:14:05 -0500

    updated readme file to include application runtime demo

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-TOP_DIR=.
-BIN_DIR=${TOP_DIR}/bin
-SRC_DIR=${TOP_DIR}/src
+# configure bash environment
 
-rm -rf ${BIN_DIR}
-mkdir -p ${BIN_DIR}
-cmake -B${BIN_DIR} -H${SRC_DIR}
-make -C ${BIN_DIR}
+set -o errexit -o pipefail -o noclobber -o nounset
+
+# declare project structure
+
+PROJECT_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BIN_DIR=${PROJECT_ROOT_DIR}/bin
+SRC_DIR=${PROJECT_ROOT_DIR}/src
+
+# build project starting with a clean slate
+
+rm -rf "${BIN_DIR}"
+mkdir -p "${BIN_DIR}"
+cmake -B"${BIN_DIR}" -H"${SRC_DIR}"
+cmake --build "${BIN_DIR}"
